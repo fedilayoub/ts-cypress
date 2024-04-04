415eccdcbf4272a7732b560d68dd1e6638d200e1
fedilayoub <ayoubzahi2016@gmail.com>
2024-04-04 23:52:33 +0000

    fixed schema issue

--- src/lib/supabase/schema.ts
+++ src/lib/supabase/schema.ts
@@ -1,1 +1,49 @@
-export {}
+import { pgTable, uuid, timestamp, text } from 'drizzle-orm/pg-core';
+
+export const workspaces = pgTable('workspaces', {
+  id: uuid('id').defaultRandom().primaryKey().notNull(),
+  createdAt: timestamp('created_at', {
+    withTimezone: true,
+    mode: 'string',
+  }),
+  worksapceOwner: uuid('workspace_owner').notNull(),
+  title: text('title').notNull(),
+  iconId: text('icon_id').notNull(),
+  data: text('data'),
+  inTrash: text('in_trash'),
+  logo: text('logo'),
+  bannerUrl: text('banner_url'),
+
+});
+
+export const folders = pgTable('folders', {
+    id: uuid('id').defaultRandom().primaryKey().notNull(),
+  createdAt: timestamp('created_at', {
+    withTimezone: true,
+    mode: 'string',
+  }),
+  workspaceId: uuid('workspace_id').references(() => workspaces.id, {
+      onDelete: 'cascade',
+  }),
+  title: text('title').notNull(),
+  iconId: text('icon_id').notNull(),
+  data: text('data'),
+  inTrash: text('in_trash'),
+  bannerUrl: text('banner_url'),
+})
+
+export const files = pgTable('files', {
+    id: uuid('id').defaultRandom().primaryKey().notNull(),
+  createdAt: timestamp('created_at', {
+    withTimezone: true,
+    mode: 'string',
+  }),
+  title: text('title').notNull(),
+  iconId: text('icon_id').notNull(),
+  data: text('data'),
+  inTrash: text('in_trash'),
+  bannerUrl: text('banner_url'),
+  folderId: uuid('folder_id').references(() => folders.id, {
+      onDelete: 'cascade',
+  })
+})
